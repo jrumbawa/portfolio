a25e3424c984ca530d574a8498ee4e7739b3f0de
= <rumbawajomar@gmail.com>
2022-05-09 11:07:42 -0700

    add Navbar component

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Navbar from '../components/Navbar'
+
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Hello Next.js!</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   )
 }
