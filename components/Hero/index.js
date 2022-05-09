26e60a086ab75a9e37267a2a1e23682e8fedaefc
= <rumbawajomar@gmail.com>
2022-05-09 11:49:42 -0700

    create Hero component and styles

---
+++ components/Hero/index.js
@@ -0,0 +1,12 @@
+export default function Hero() {
+  return (
+    <header>
+      <p>Front-End Developer</p>
+      <h1>Title</h1>
+      <div>
+        <a href="#">Work</a>
+        <a href="#">Resume</a>
+      </div>
+    </header>
+  )
+}
