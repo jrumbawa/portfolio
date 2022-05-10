bacbc6c3b38a16f545a29b02fb541dbc634c2ebf
= <rumbawajomar@gmail.com>
2022-05-09 22:07:43 -0700

    rename index to Navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -3,7 +3,10 @@ import styles from './Navbar.module.css'
 export default function Navbar() {
   return (
     <nav className={styles.nav}>
-      <p>Jomar</p>
+      <p className={styles.navLogo}>
+        Jomar.<span className={styles.navLogoSpan}>Rumbawa</span>
+      </p>
+      <i className={`bi bi-justify ${styles.navButton}`}></i>
       <ul className={styles.navList}>
         <li>Skills</li>
         <li>Work</li>
