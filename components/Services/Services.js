7b5d481efe78b4bb543d04cd4f6be2649b85e205
= <rumbawajomar@gmail.com>
2022-05-15 09:55:58 -0700

    add lorem text to service

--- components/Services/Services.js
+++ components/Services/Services.js
@@ -1,9 +1,31 @@
 import styles from './Services.module.css'
 
-function Services() {
+export default function Services() {
   return (
     <section className={styles.services}>
       <h2>Quality Services</h2>
+      <div className={styles.services}>
+        <article className={styles.service}>
+          <p className={styles.serviceTitle}>
+            <span className={styles.blueText}>01.</span>Frontend Development
+          </p>
+          <p className={styles.serviceDescription}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+            ullam sit eaque odit quo earum numquam velit veritatis, repellendus
+            molestias.
+          </p>
+        </article>
+        <article className={styles.service}>
+          <p className={styles.serviceTitle}>
+            <span className={styles.blueText}>02.</span>Frontend Design
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate
+            ullam sit eaque odit quo earum numquam velit veritatis, repellendus
+            molestias.
+          </p>
+        </article>
+      </div>
     </section>
   )
 }
