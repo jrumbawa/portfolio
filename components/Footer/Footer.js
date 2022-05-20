399b245c9cb5f931e2eb5602c625fd649d843085
= <rumbawajomar@gmail.com>
2022-05-19 23:32:05 -0700

    add footer paragraph

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,5 +1,9 @@
 import styles from './Footer.module.css'
 
 export default function Footer() {
-  return <footer></footer>
+  return (
+    <footer>
+      <p>Copyright &copy; 2022. Designed by Jomar. All rights reserved.</p>
+    </footer>
+  )
 }
